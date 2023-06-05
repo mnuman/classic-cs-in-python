9bcab016fa4b43d7f352d58ce2da9f2c8b9e2070
Milco Numan <milco.numan@gmail.com>
2023-06-05 07:38:33 +0200

    Reformatted

--- H3/csp.py
+++ H3/csp.py
@@ -16,36 +16,41 @@
 from typing import Generic, TypeVar, Dict, List, Optional
 from abc import ABC, abstractmethod
 
-V = TypeVar('V')  # variable type
-D = TypeVar('D')  # domain type
+Variable = TypeVar("Variable")  # variable type
+Domain = TypeVar("Domain")  # domain type
 
 
 # Base class for all constraints
-class Constraint(Generic[V, D], ABC):
+class Constraint(Generic[Variable, Domain], ABC):
     # The variables that the constraint is between
-    def __init__(self, variables: List[V]) -> None:
+    def __init__(self, variables: List[Variable]) -> None:
         self.variables = variables
 
     # Must be overridden by subclasses
     @abstractmethod
-    def satisfied(self, assignment: Dict[V, D]) -> bool:
+    def satisfied(self, assignment: Dict[Variable, Domain]) -> bool:
         ...
 
 
 # A constraint satisfaction problem consists of variables of type V
 # that have ranges of values known as domains of type D and constraints
 # that determine whether a particular variable's domain selection is valid
-class CSP(Generic[V, D]):
-    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
-        self.variables: List[V] = variables # variables to be constrained
-        self.domains: Dict[V, List[D]] = domains # domain of each variable
-        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+class CSP(Generic[Variable, Domain]):
+    def __init__(
+        self, variables: List[Variable], domains: Dict[Variable, List[Domain]]
+    ) -> None:
+
+        self.variables: List[Variable] = variables  # variables to be constrained
+        self.domains: Dict[Variable, List[Domain]] = domains  # domain of each variable
+        self.constraints: Dict[Variable, List[Constraint[Variable, Domain]]] = {}
+
+        # cross-check
         for variable in self.variables:
             self.constraints[variable] = []
             if variable not in self.domains:
                 raise LookupError("Every variable should have a domain assigned to it.")
 
-    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+    def add_constraint(self, constraint: Constraint[Variable, Domain]) -> None:
         for variable in constraint.variables:
             if variable not in self.variables:
                 raise LookupError("Variable in constraint not in CSP")
@@ -54,28 +59,32 @@ class CSP(Generic[V, D]):
 
     # Check if the value assignment is consistent by checking all constraints
     # for the given variable against it
-    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+    def consistent(self, variable: Variable, assignment: Dict[Variable, Domain]) -> bool:
         for constraint in self.constraints[variable]:
             if not constraint.satisfied(assignment):
                 return False
-        return True
+        return True                 # fall-through if no violations have been found
 
-    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+    def backtracking_search(
+        self, assignment: Dict[Variable, Domain] = {}
+    ) -> Optional[Dict[Variable, Domain]]:
         # assignment is complete if every variable is assigned (our base case)
         if len(assignment) == len(self.variables):
             return assignment
 
         # get all variables in the CSP but not in the assignment
-        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+        unassigned: List[Variable] = [v for v in self.variables if v not in assignment]
 
         # get the every possible domain value of the first unassigned variable
-        first: V = unassigned[0]
+        first: Variable = unassigned[0]
         for value in self.domains[first]:
             local_assignment = assignment.copy()
             local_assignment[first] = value
             # if we're still consistent, we recurse (continue)
             if self.consistent(first, local_assignment):
-                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+                result: Optional[Dict[Variable, Domain]] = self.backtracking_search(
+                    local_assignment
+                )
                 # if we didn't find the result, we will end up backtracking
                 if result is not None:
                     return result
